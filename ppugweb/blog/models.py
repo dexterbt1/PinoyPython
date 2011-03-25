f1f9595ff65d658639e9ab8bf1d8e2d41e4eb6af
dexterbt1 <dtady@cpan.org>
2011-03-26 01:31:43 +0800

    added minimal Meta info for blog.models

--- ppugweb/blog/models.py
+++ ppugweb/blog/models.py
@@ -1,15 +1,28 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
 class BlogEntry(models.Model):
     title = models.CharField(max_length=200)
     publish_date = models.DateTimeField('publish_date')
     content = models.TextField()
+
+    class Meta:
+        verbose_name = u"Blog Entry"
+        verbose_name_plural = u"Blog Entries"
+
     
 class Category(models.Model):
     category_name = models.CharField(max_length=150)
 
+    class Meta:
+        verbose_name = u"Category"
+        verbose_name_plural = u"Categories"
+
+
 class Tag(models.Model):
     tag_name = models.CharField(max_length=150)
+
     
 class Author(models.Model):
     name = models.CharField(max_length=200)
